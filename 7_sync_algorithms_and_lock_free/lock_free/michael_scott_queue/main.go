14ab8a48e5c61534155203f5e135a13f9058691e
nextbug <nextbug@ya.ru>
2024-10-18 19:54:01 +0300

    edit michael_scott_queue

--- 7_sync_algorithms_and_lock_free/lock_free/michael_scott_queue/main.go
+++ 7_sync_algorithms_and_lock_free/lock_free/michael_scott_queue/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type item struct {
-	value int
+	value string
 	next  unsafe.Pointer
 }
 
@@ -16,74 +16,74 @@ type Queue struct {
 	tail unsafe.Pointer
 }
 
-func NewQueue() Queue {
+func New() *Queue {
 	dummy := &item{}
-	return Queue{
+	return &Queue{
 		head: unsafe.Pointer(dummy),
 		tail: unsafe.Pointer(dummy),
 	}
 }
 
-func (q *Queue) Push(value int) {
-	// create new node
+func (q *Queue) Push(value string) {
+	// Создание нового узла
 	node := &item{value: value}
 
 	for {
-		// read tail and next reference
+		// Чтение указателей tail и next
 		tail := atomic.LoadPointer(&q.tail)
 		next := atomic.LoadPointer(&(*item)(tail).next)
 
-		// if tail is the same
+		// Если tail не изменился
 		if tail == atomic.LoadPointer(&q.tail) {
+			// гарантия, что текущий узел последний в очереди
 			if next == nil {
-				// no need to fix tail, try CAS
+				// проверяем, что next все еще nil и если это так обновляем его на node
 				if atomic.CompareAndSwapPointer(&(*item)(tail).next, next, unsafe.Pointer(node)) {
-					// CAS was successful, try to fix tail
+					// CAS был успешен, фиксируем tail
 					atomic.CompareAndSwapPointer(&q.tail, tail, unsafe.Pointer(node))
 					return
 				}
 			} else {
-				// try to fix tail from other goroutine
+				// Пробуем обновить tail, если другая горутина изменила его
 				atomic.CompareAndSwapPointer(&q.tail, tail, next)
 			}
 		}
 	}
 }
 
-func (q *Queue) Pop() int {
+func (q *Queue) Pop() string {
 	for {
+		// Загружаем указатели head, tail и next
 		head := atomic.LoadPointer(&q.head)
 		tail := atomic.LoadPointer(&q.tail)
 		next := atomic.LoadPointer(&(*item)(head).next)
 
-		// if head is the same
-		if head == atomic.LoadPointer(&q.head) {
-			// if head and tail are some node
-			if head == tail {
-				if next == nil {
-					// queue contains only dummy node
-					return -1
-				} else {
-					// otherwise tail must be fixed
-					atomic.CompareAndSwapPointer(&q.tail, tail, next)
-				}
-			} else {
-				// try to pop item
-				value := (*item)(next).value
-				if atomic.CompareAndSwapPointer(&q.head, head, next) {
-					return value
-				}
+		// очередь не пустая
+		if head != tail {
+			// попробуем вытащить value
+			value := (*item)(next).value
+
+			if atomic.CompareAndSwapPointer(&q.head, head, next) {
+				return value
 			}
+		} else {
+			// если голова и хвост — это какой-то узел
+			if next == nil {
+				// очередь содержит только фиктивный узел
+				return ""
+			}
+			// иначе хвост надо зафиксировать
+			atomic.CompareAndSwapPointer(&q.tail, tail, next)
 		}
 	}
 }
 
 func main() {
-	queue := NewQueue()
+	queue := New()
 
-	queue.Push(1)
-	queue.Push(2)
-	queue.Push(3)
+	queue.Push("A")
+	queue.Push("B")
+	queue.Push("C")
 
 	fmt.Println(queue.Pop())
 	fmt.Println(queue.Pop())
